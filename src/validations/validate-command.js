31d99e3df3796f541dbc22824336fe12218667bc
Luka Mihaldinec <luka.mihaldinec@gmail.com>
2021-04-18 14:06:20 +0200

    [REMOVE] Removed validating command keywords in validate-command

--- src/validations/validate-command.js
+++ src/validations/validate-command.js
@@ -1,24 +1,10 @@
-const botCommands = require('../../botCommands.json');
-
-
-function validateIsBotCommand(msg) {
-    const message = msg.content.toLowerCase();
-    const isValidBotCommand = message.startsWith("!karta");
-    
-    // TODO:
-    // Expand to check for each of the available
-
-    return isValidBotCommand;
-}
-
 function validateIsNotBotUser(msg) {
     return msg.author != msg.author.bot;
 }
 
 function isValid(msg) {
-    const isValidBotCommand = validateIsBotCommand(msg);
     const isNotBotUser = validateIsNotBotUser(msg);
-    const isValid = isValidBotCommand && isNotBotUser;
+    const isValid = isNotBotUser;
 
     return isValid;
 }
